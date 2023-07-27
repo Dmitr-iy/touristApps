b0fe9117e8680dff4e0aedd1cdfe5ead5d6c25e6
dmitriy <kosheld@inbox.ru>
2023-07-27 18:09:47 +0500

    Add filter email

--- pereval/views.py
+++ pereval/views.py
@@ -1,3 +1,4 @@
+import django_filters
 from rest_framework import viewsets
 from .serializers import AddedSerializer
 from .models import Added
@@ -7,3 +8,5 @@ class AddedViewSet(viewsets.ModelViewSet):
     """Adding method post"""
     queryset = Added.objects.all()
     serializer_class = AddedSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ["user__email"]
